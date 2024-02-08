025b07bd04122d0c4ede3282ee2c9eceba101d0d
joshua-demo <116464215+joshua-demo@users.noreply.github.com>
2024-02-08 15:51:34 -0800

    done

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,39 +1,72 @@
 'use client';
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../lib/helpers/users";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Page() {
   const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState('');
+  const [error, setError] = useState(false);
 
-  useEffect(() => {
-    async function loadData() {
-      const userData = await getAllUsers();
-      setUsers(userData);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("new user is", newUser);
+    try {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: newUser })
+      });
+
+      if (response.ok) {
+        setError(false);
+        loadData();
+      } else {
+        setError(true)
+      }
+
+    } catch (error) {
+      setError(true);
     }
+  };
+  async function loadData() {
+    const userData = await getAllUsers();
+    setUsers(userData);
+  }
+  useEffect(() => {
     loadData()
   }, [])
   return (
-    <>
+    <div className="mx-10 ml-4">
       <div className='text-center'>
         <h1 className="text-3xl">Admin Page</h1>
       </div>
-      <ul className="mt-10">
+      <input onChange={(e) => setNewUser(e.target.value)} placeholder="user to add" className="p-2 text-white bg-black border rounded-lg"></input>
+      <button onClick={handleSubmit} className="p-2 ml-4 border rounded-xl">Click to add user</button>
+      {error && (
+        <p>Error adding user</p>
+      )}
+      <ul>
         {/* Map users array and render user IDs and usernames */}
         {users.map(user => (
-          <div key={user.userID} className="m-2 text-center border ">
+          <div key={user.userID} className="p-2 mt-2 text-center border">
             <div>
               <div>
                 ID: {user.userID}
-                <span className="ml-5">Username: {user.username}</span>
+              </div>
+              <div>
+                Username: {user.username}
               </div>
               <span>
                 <Link className="ml-8 text-blue-500 hover:underline" href={`/admin/${user.userID}`}>View</Link>
               </span>
+              <button></button>
             </div>
           </div>
         ))}
       </ul>
-    </>
+    </div>
   )
 }
